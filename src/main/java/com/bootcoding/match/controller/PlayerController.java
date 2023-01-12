265f42393fe83b89a15d9f69ca120fbafef80d0d
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-12 14:33:00 +0530

    code implemented ..

--- src/main/java/com/bootcoding/match/controller/PlayerController.java
+++ src/main/java/com/bootcoding/match/controller/PlayerController.java
@@ -2,9 +2,8 @@ package com.bootcoding.match.controller;
 import com.bootcoding.match.model.Player;
 import com.bootcoding.match.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -20,19 +19,24 @@ public class PlayerController
 //    }
 
 
+//    @GetMapping("/player/{name}")
+//    public Player getPlayerName(@PathVariable("name") String name)
+//    {
+//     List<Player> allPlayer = playerService.getAllPlayers();
+//        for(Player player : allPlayer)
+//        {
+//            if(player.getName().equals(name))
+//            {
+//                return player;
+//            }
+//        }
+//        return null;
+//    }
 
 
-    @GetMapping("/player/{name}")
-    public Player getPlayerName(@PathVariable("name") String name)
-    {
-     List<Player> allPlayer = playerService.getAllPlayers();
-        for(Player player : allPlayer)
-        {
-            if(player.getName().equals(name))
-            {
-                return player;
-            }
-        }
-        return null;
+    @PostMapping("/player")
+    public String createEmployee(@RequestBody Player player) {
+        System.out.println("printing the player object.."+player);
+        return "Successfully Added..";
     }
 }
